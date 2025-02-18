bc199d27b2dcec2effe2ce10b3fd2c7d91fe2322
GitzauTT <giozau1@gmail.com>
2025-02-18 13:05:51 +0400

    SmartAcademy first commit

---
+++ forGithubCommit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  forGithubCommit
+//
+//  Created by Giorgi Zautashvili on 18.02.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
